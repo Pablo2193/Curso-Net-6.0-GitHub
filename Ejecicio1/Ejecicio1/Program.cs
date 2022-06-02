23650960de3847320761b4459273d64ddc89a13d
pablonicovillalba2193@gmail.com <pablonicovillalba2193@gmail.com>
2022-06-02 20:32:13 -0300

    Subo el ejercicio numero 1 bucle con while

---
+++ Ejecicio1/Ejecicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+var contadorDeVueltas = 0;
+var respuesta = "S";
+
+while (respuesta.ToUpper()!= "N")
+{
+    contadorDeVueltas++;
+    
+    Console.Clear();
+    Console.WriteLine("Vuelta numero " + contadorDeVueltas);
+    Console.WriteLine("Ingrese su nombre por favor");
+    var nombre = Console.ReadLine();
+    Console.WriteLine($"Hola {nombre} eres bienvenido");
+    Console.WriteLine("Quiere continuar S/N?");
+    respuesta = Console.ReadLine();
+    if (respuesta.ToUpper()=="N")
+    {
+        Console.WriteLine("Programa finalizado correctamente");
+    }
+    if (respuesta.ToUpper() != "N" && respuesta.ToUpper() != "S")
+    {
+        Console.WriteLine("Opción no valida");
+        break;
+    }
+}
+
